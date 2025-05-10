aa5d1eab7cc24232ced2038090f9ec1f257c935b
Rilzy <144298570+RIlz27@users.noreply.github.com>
2025-05-10 09:09:25 +0700

    m

---
+++ src/components/TitleComponent.tsx
@@ -0,0 +1,36 @@
+
+interface TitleComponentProps {
+  pt: number;
+  pb: number;
+  title: String;
+  subTitle?: String;
+  withSub?: boolean;
+}
+
+const TitleComponent = ({ pt, pb, title, withSub, subTitle}: TitleComponentProps) => {
+  return (
+    <div className={`flex justify-center pt-[${pt}px] pb-[${pb}px]`}>
+      <div className="flex flex-col gap-3">
+        <h1
+          className="text-center font-bold text-4xl"
+          data-aos="fade-up"
+          data-aos-duration="1000"
+        >
+          {title}
+        </h1>
+        {withSub && (
+          <p
+            className="text-center"
+            data-aos="fade-up"
+            data-aos-duration="2000"
+            data-aos-delay="200"
+          >
+            {subTitle}
+          </p>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default TitleComponent;
